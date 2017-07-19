5c12ee206565c98e754d2763be151b594fe34f81
com-ArgonITLtd-kr <30293673+com-ArgonITLtd-kr@users.noreply.github.com>
2017-07-19 18:34:52 +0200

    Add files via upload

---
+++ HelloEarth3.java
@@ -0,0 +1,9 @@
+/**
+ * The HelloEarthApp class implements an application that
+ * simply prints "Hello Earth!" to standard output.
+ */
+class HelloEarthApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Earth"); /** Display the string.*/
+	}
+}
